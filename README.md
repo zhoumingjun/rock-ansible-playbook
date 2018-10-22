143a829530ed0e05f073b31b60b42747027c8187
zhoumingjun <zhoumingjun@gmail.com>
2018-10-23 01:58:06 +0800

    update

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ ansible-vault decrypt host_vars/* --vault-id @vaultid
 ```
  
 ##  install rock64
-ansible-playbook rock64.yml -i hosts --user rock64 --ask-become-pass  --vault-id @vaultid  -vv
+ansible-playbook rock64.yml -i hosts --user zhoumingjun --ask-become-pass  --vault-id @vaultid  -vv
 
 ## install rpizw
 ansible-playbook rpizw.yml -i hosts --user pi --ask-become-pass  --vault-id @vaultid  -vv
--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > roles/chinadns/files/chnroute.txt
-curl  https://cokebar.github.io/gfwlist2dnsmasq/dnsmasq_gfwlist_ipset.conf -o roles/dnsmasq/files/dnsmasq_gfwlist_ipset.conf
+curl  https://cokebar.github.io/gfwlist2dnsmasq/dnsmasq_gfwlist_ipset.conf -o roles/dnsmasq/templates/dnsmasq_gfwlist_ipset.conf.j2
+sed -i '' 's/5353/{{ chinadns.port }}/g'  roles/dnsmasq/templates/dnsmasq_gfwlist_ipset.conf.j2
  
  
  
